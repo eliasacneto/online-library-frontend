4c266fe15b79afd0913a24b7c962fa35533d820a
Elias Neto <netoeac.dev@gmail.com>
2024-04-20 10:56:45 -0300

    update api url path

--- src/api/BookService.js
+++ src/api/BookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://online-library-backend-blond.vercel.app/"
 
 export class BookService {
     static getAllBooks() {
